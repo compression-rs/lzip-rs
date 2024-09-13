b565793e1db79d9ce9cd07860e7013b7dd58b3f6
Ivan Krivosheev <py.krivosheev@gmail.com>
2024-09-13 11:19:07 +0300

    Upgrade lzip to 1.14

--- lzip-sys/build.rs
+++ lzip-sys/build.rs
@@ -1,12 +1,10 @@
 use std::env;
 
-extern crate cc;
-
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     cc::Build::new()
-        .file("lzlib-1.13/lzlib.c")
+        .file("lzlib-1.14/lzlib.c")
         .out_dir(&out_dir)
         .compile("liblz.a");
 
